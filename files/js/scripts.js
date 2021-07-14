f3abd21912810a915475505a6dbfc3b7d6a4b8d3
CharlesConagher <mihail.romashenko03@mail.ru>
2021-07-14 18:39:00 +0300

    first commit

---
+++ files/js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.shop__gallery').slick({
+        arrows:true,
+        dots:false,
+        slidesToShow: 3,
+    });
+    $('.intro__grid-gallery').slick({
+        arrows:true,
+        dots:false,
+        slideToShow: 1,
+        variableWidth: false,
+        
+    })
+});
